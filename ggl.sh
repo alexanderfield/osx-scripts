8f56e4ea03af6a34f4425d0b1c9be4d4cbf39368
Alex Field <alex@Alexs-MBP.home>
2015-07-18 19:09:22 -0400

    initial commit

---
+++ ggl.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Author: Alexander Field
+# License
+#
+#
+
+if [ "$#" -eq 0 ]; then
+    echo "what do you want to search for?"
+    read SEARCH
+else
+    SEARCH="$@"
+fi
+
+q=""
+i=0
+
+for WORD in $SEARCH
+do
+   if [ "$i" -eq "0" ]
+   then
+      q+=$WORD
+   else
+      q+="+"$WORD
+   fi
+   i+=1
+done
+
+open -a Safari http://www.google.com/search?q=$q
